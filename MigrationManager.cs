2736ff01897a00aab9303f883e14efdcb7a91367
DruSatori <dru@druware.com>
2024-01-04 19:47:06 -0500

    Reconfigured to support multiple contexts
    Added full MSSQL / Azure support

--- MigrationManager.cs
+++ MigrationManager.cs
@@ -1,48 +1,57 @@
-﻿using System;
+﻿using Druware.Server;
+using Druware.Server.Content;
 using Microsoft.EntityFrameworkCore;
 
 namespace Druware.API
 {
     public static class MigrationManager
     {
+
         public static WebApplication MigrateDatabase(this WebApplication host)
         {
-            using (var scope = host.Services.CreateScope())
-            {
+            // can we get to a settings object? 
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+            var settings = new AppSettings(configuration);
+            
+            using var scope = host.Services.CreateScope();
 
-                using (var appContext = scope.ServiceProvider.GetRequiredService<Druware.Server.ServerContext>())
-                {
-                    try
-                    {
-                        appContext.Database.Migrate();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Log errors or do anything you think it's needed
-                        Console.Error.WriteLine("MigrateDatabase Failed: {0}", ex.Message);
-                        throw;
-                    }
-                }
-                using (var appContext = scope.ServiceProvider.GetRequiredService<Druware.Server.Content.ContentContext>())
-                {
-                    try
-                    {
-                        appContext.Database.Migrate();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Log errors or do anything you think it's needed
-                        Console.Error.WriteLine("MigrateDatabase Failed: {0}", ex.Message);
-                        throw;
-                    }
-                }
+            // get the correct context.
+            DbContext? serverContext;
+            DbContext? contentContext;
+            switch (settings.DbType)
+            {
+                case DbContextType.Microsoft:
+                    serverContext = scope.ServiceProvider
+                        .GetRequiredService<ServerContextMicrosoft>();
+                    contentContext = scope.ServiceProvider
+                        .GetRequiredService<ContentContextMicrosoft>();
+                    break;
+                case DbContextType.PostgreSql:
+                    serverContext = scope.ServiceProvider
+                        .GetRequiredService<ServerContextPostgreSql>();
+                    contentContext = scope.ServiceProvider
+                        .GetRequiredService<ContentContextPostgreSql>();
+                    break;
+                default:
+                    throw new Exception("Unknown DbType");
             }
+//            serverContext = scope.ServiceProvider
+//                .GetRequiredService<ServerContext>();
+//            contentContext = scope.ServiceProvider
+//                .GetRequiredService<ContentContext>();
+
+            if (serverContext == null) 
+                throw new Exception("No ServerContext is available");
+            if (contentContext == null) 
+                throw new Exception("No ContentContext is available");
+            
+            serverContext.Database.Migrate();
+            contentContext.Database.Migrate();
 
             // Followup with Security Roles that we want in place
-            using (var scope = host.Services.CreateScope())
-            using (Druware.Server.ServerContext context = scope.ServiceProvider.GetRequiredService<Druware.Server.ServerContext>())
-                if (context != null)
-                    Druware.Server.Content.ContentContext.ConfigureSecurityRoles(context);
+            // -- contentContext.ConfigureSecurityRoles(serveContext);
 
             return host;
         }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 using Druware.API;
 using Druware.Server;
+using Druware.Server.Content;
 using Druware.Server.Entities;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 // dotnet ef migrations add Init
 
-const string connectionString = "Host=localhost;Database=druware;Username=postgres;Password=gr8orthan0";
+const string connectionString = "Host=localhost;Database=druware;Username=postgres;Password=blahblahblah!";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +17,10 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false)
     .Build();
+
+var settings = new AppSettings(configuration);
+var cs = (string.IsNullOrEmpty(settings.ConnectionString)) ? 
+    connectionString : settings.ConnectionString;
 
 // Will probably want AutoMapper ( NuGet: AutoMapper )
 builder.Services.AddAutoMapper(typeof(Program));
@@ -29,28 +33,47 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 
 // DbContext stuff
 // Repeat the following for each library context added
+// bear in mind, this will be different depending upon the backend, so get the 
+// backend right first.
 
-builder.Services.AddDbContext<Druware.Server.ServerContext>(conf =>
+switch (settings.DbType)
 {
-    conf.UseNpgsql(connectionString);
-});
-builder.Services.AddDbContext<Druware.Server.Content.ContentContext>(conf =>
-{
-    conf.UseNpgsql(connectionString);
-});
-
+    case DbContextType.Microsoft:
+        builder.Services.AddDbContext<ServerContext>(
+            conf => conf.UseSqlServer(cs));
+        builder.Services.AddDbContext<ContentContext>(
+            conf => conf.UseSqlServer(cs));
+        
+        builder.Services.AddDbContext<ServerContextMicrosoft>(
+            conf => conf.UseSqlServer(cs));
+        builder.Services.AddDbContext<ContentContextMicrosoft>(
+            conf => conf.UseSqlServer(cs));
+        break;
+    case DbContextType.PostgreSql:
+        builder.Services.AddDbContext<ServerContext>(
+            conf => conf.UseNpgsql(cs));
+        builder.Services.AddDbContext<ContentContext>(
+            conf => conf.UseNpgsql(cs));
+        
+        builder.Services.AddDbContext<ServerContextPostgreSql>(
+            conf => conf.UseNpgsql(cs));
+        builder.Services.AddDbContext<ContentContextPostgreSql>(
+            conf => conf.UseNpgsql(cs));
+        break;
+    default:
+        throw new ArgumentOutOfRangeException();
+}
 
 // This API is using the IdentityFramework, so go ahead and configure that as
 // well, note, this is all implemented in the OpenBCM.API.Authentication.dll
-// library
-
+// library 
 builder.Services.AddIdentity<User, Role>(config =>
-{
-    config.Password.RequiredLength = 8;
-    config.User.RequireUniqueEmail = true;
-    config.SignIn.RequireConfirmedEmail = true;
-})
-    .AddEntityFrameworkStores<Druware.Server.ServerContext>()
+    {
+        config.Password.RequiredLength = 8;
+        config.User.RequireUniqueEmail = true;
+        config.SignIn.RequireConfirmedEmail = true;
+    })
+    .AddEntityFrameworkStores<ServerContext>()
     .AddDefaultTokenProviders();
 
 builder.Services.ConfigureApplicationCookie(config =>
@@ -58,8 +81,6 @@ builder.Services.ConfigureApplicationCookie(config =>
     config.Cookie.Name = "Druware.API";
     config.LoginPath = "/login/";
 });
-
-
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
