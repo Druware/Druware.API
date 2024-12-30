cda3a2da13d13188f880f90289b38b85bbf14731
DruSatori <dru@druware.com>
2024-12-29 23:58:13 -0500

    Updated to .net 8

--- Program.cs
+++ Program.cs
@@ -102,16 +102,9 @@ builder.Services.ConfigureApplicationCookie(config =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
